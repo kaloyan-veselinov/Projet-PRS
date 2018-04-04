10cb6d1c7cf9d7e963e205c9468fcc959d9b8727
Kaloyan Veselinov <kvesselinov@gmail.com>
2018-04-04 16:47:59 +0200

    socket.h et makefile

--- src/socket.c
+++ src/socket.c
@@ -1,20 +1,5 @@
 #include "socket.h"
 
-int open_socket(int mode) {
-
-  int sock;
-  sock = socket(AF_INET, mode,0);
-  if (sock < 0) {
-    fprintf(stderr, "Erreur d'ouverture de la socket avec retour de %d\n", sock);
-    exit(-1);
-  }
-
-  int reuse = 1; /* PERMET LA REUTILISATION DE L'ADRESSE */
-  setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
-
-  return sock;
-}
-
 struct sockaddr_in init_addr(int port, int addr) {
   struct sockaddr_in my_addr;
   memset((char*)&my_addr,0,sizeof(my_addr));
@@ -25,89 +10,72 @@ struct sockaddr_in init_addr(int port, int addr) {
   return my_addr;
 }
 
-void my_bind(int socket, struct sockaddr* addr) {
-  int res = bind (socket, addr , sizeof(*addr));
-  if (res < 0) {
+int create_socket(int port) {
+    struct sockaddr_in adresse = init_addr(port, INADDR_ANY);
+    int desc = socket(AF_INET, SOCK_DGRAM, 0);
+    int valid = 1;
+
+    if (desc < 0) {
+        perror("cannot create socket\n");
+        return -1;
+    }
+
+    setsockopt(desc, SOL_SOCKET, SO_REUSEADDR, &valid, sizeof(int));
+
+    my_bind(desc,(struct sockaddr *)&adresse);
+
+    return desc;
+}
+
+int my_bind(int socket, struct sockaddr* addr) {
+  if (bind (socket, addr , sizeof(*addr)) < 0) {
     fprintf(stderr, "Erreur de bind de la socket\n");
-    exit(-1);
+    return EXIT_FAILURE;
   }
-}
-
-int my_connect(int desc, struct sockaddr* adresse) {
-  socklen_t addrLen = sizeof(*adresse);
-
-  char msg[RCVSIZE] = "SYN";
-  sendto(desc,msg,strlen(msg),0, adresse, addrLen);
-
-  printf("SYN envoyé.\n");
-
-  memset(msg, 0, RCVSIZE);
-
-  recv(desc, msg, sizeof(msg),0);
-  if (strncmp("SYN-ACK", msg, strlen("SYN-ACK")) != 0) {
-    fprintf(stderr, "Connexion refusée\n");
-    return -1;
-  }
-
-  char sport[RCVSIZE];
-  strcpy(sport, msg+strlen("SYN-ACK"));
-  printf("Valeur reçue en sport : %s\n", sport);
-
-  int port = atoi(sport);
-
-  printf("SYN-ACK reçu\n");
-
-  strcpy(msg,"ACK");
-  sendto(desc,msg,strlen(msg),0, (struct sockaddr*) adresse, addrLen);
-
-
-  printf("ACK envoyé\n");
-
-  return port;
-
+  return EXIT_SUCCESS;
 }
 
 
-
-int my_accept(int desc, struct sockaddr_in* addr, struct sockaddr_in* client) {
-
-  socklen_t clientLen = sizeof(client);
-  socklen_t addrLen = sizeof(*addr);
+int my_accept(int desc, struct sockaddr_in* addr) {
 
   char msg[RCVSIZE];
+  memset(msg, '\0', RCVSIZE);
 
-  int msgSize= recvfrom(desc,msg,RCVSIZE,0, &client, &clientLen);
+  socklen_t addr_len = sizeof(addr);
+  memset(addr, 0, addr_len);
+
+
+  if(recvfrom(desc,msg,RCVSIZE,0, (struct sockaddr *)&addr, &addr_len)==-1){
+    perror("Error receiving on connection socket");
+    return EXIT_FAILURE;
+  }
 
   if (strncmp("SYN", msg, strlen("SYN")) != 0) {
-    fprintf(stderr, "Connexion refusée\n");
-    return -1;
+    perror("Mauvais message de connexion\n");
+    return EXIT_FAILURE;
   }
-
   printf("SYN reçu.\n");
 
-  int util_sock = open_socket(SOCK_DGRAM);
-  my_bind(util_sock, (struct sockaddr*) addr);
+  struct sockaddr_in data_addr;
+  int data_desc = create_socket(0);
+  getsockname(data_desc, (struct sockaddr*) &data_addr, &addr_len);
 
-  getsockname(util_sock, (struct sockaddr*) addr, &addrLen);
+  printf("Port : %d\n", data_addr.sin_port);
 
-  printf("Port : %d\n", addr->sin_port);
-  int out = addr->sin_port;
-
-  strcpy(msg,"SYN-ACK");
-  sprintf(msg+ strlen("SYN-ACK"), "%d", out);
-  sendto(desc,msg,strlen(msg),0, (struct sockaddr*) &client, clientLen);
-
+  sprintf(msg, "SYN-ACK%04d", ntohs(data_addr.sin_port));
+  sendto(desc,msg,strlen(msg)+1,0, (struct sockaddr*) &addr, addr_len);
   printf("SYN-ACK envoyé\n");
 
-  recvfrom(desc,msg,RCVSIZE,0, &client, &clientLen);
-
-  if (strncmp("ACK", msg, strlen("ACK")) != 0) {
-    fprintf(stderr, "Connexion refusée, reçu : %.3s\n", msg);
-    return -1;
+  if(recvfrom(desc,msg,RCVSIZE,0, (struct sockaddr*) &addr, &addr_len)==-1){
+    perror("Erreur de réception du ACK de connexion\n.");
+    return EXIT_FAILURE;
   }
 
+  if (strncmp("ACK", msg, strlen("ACK")) != 0) {
+    fprintf(stderr, "Mauvais message d'ACK de connexion.\n");
+    EXIT_FAILURE;
+  }
   printf("ACK reçu, connexion acceptée.\n");
 
-  return util_sock;
+  return data_desc;
 }
-
